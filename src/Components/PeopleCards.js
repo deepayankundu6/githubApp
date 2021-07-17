425a70f3be7fc73bbc745171222e572edd6c0c17
Deepayan Kundu <deepayankundu6@gmail.com>
2021-07-17 19:49:12 +0530

    fv

--- src/Components/PeopleCards.js
+++ src/Components/PeopleCards.js
@@ -1,22 +1,19 @@
 import React from "react"
 
-class PeopleCards extends React.Component {
-
-    render() {
-        console.log(this.props.Users);
-        return this.props.Users.default.map((user) => {
-            return <div class="card" style={{ margin: '1%' }}>
-                <img class="card-img-top" style={myImageStyles} src={user.avatar_url} alt="User Image" />
-                <div class="card-body">
-                    <h5 class="card-title">{user.login}</h5>
-                    {/* <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
-                    <a href={user.html_url} class="btn btn-primary">Profile Link</a>
-                </div>
-                <br></br>
+const PeopleCards = (props) => {
+    return props.Users.map((user) => {
+        return <div class="card" style={{ margin: '1%' }}>
+            <img class="card-img-top" style={myImageStyles} src={user.avatar_url} alt="User Image" />
+            <div class="card-body">
+                <h5 class="card-title">{user.login}</h5>
+                {/* <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
+                <a href={user.html_url} class="btn btn-primary">Profile Link</a>
             </div>
-        });
-    }
+            <br></br>
+        </div>
+    });
 }
+
 
 const myImageStyles = {
     borderRadius: '50%',
