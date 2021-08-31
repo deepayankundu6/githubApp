668517093d99255102ead02db5fa0e1228f1e070
Deepayan Kundu <deepayankundu6@gmail.com>
2021-08-31 12:41:33 +0530

    vfvfv

--- src/Components/UserView.js
+++ src/Components/UserView.js
@@ -1,26 +1,23 @@
-import { React, useEffect } from "react"
+import { React, useEffect, useState } from "react"
 import { useParams } from "react-router-dom";
 import axios from 'axios';
 
 function UserView() {
-
-    let userProfiledata;
+    const [userProfile, setUserProfile] = useState(Object);
     let { id } = useParams();
 
     useEffect(() => {
         axios.get(`https://api.github.com/user/${id}`).then(data => {
-
-            userProfiledata = data;
-            console.log(userProfiledata);
+            setUserProfile(data.data);
 
         }).catch(err => {
             console.log("Error occured while receiving data. ", err);
         })
+        // console.log(userProfile);
     }, [])
-
-
+    let name = userProfile.login
     return <div>
-        <h2>Hi!</h2>
+        <h2>Hi:{name}</h2>
 
     </div>
 
